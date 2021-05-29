58a02d37a8717c6f3ef3af259f4f6a71fbccb5bc
ivan-masiuk <vavolan@gmail.com>
2021-05-29 13:43:58 +0300

    hot fix: settings.py: install_apps

--- account/urls.py
+++ account/urls.py
@@ -4,9 +4,9 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
     path('password_change/',
-         auth_views.PasswordChangeView.as_view(),
+         auth_views.PasswordChangeView.as_view(template_name='registration/password_change_form.html'),
          name='password_change'),
     path('password_change/done/',
          auth_views.PasswordChangeDoneView.as_view(),
