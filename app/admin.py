a2f3b6fe02d68b260067549c0b8747807b2482d1
Danil Vladimirov <danilvladimirovnkk@gmail.com>
2021-05-29 18:30:36 +0300

    fix

--- app/admin.py
+++ app/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from app.models import (Group,
                         Week,
                         Lesson,
-                        Day)
+                        Day,
+                        Subject)
 
 admin.site.register(Group)
 admin.site.register(Week)
 admin.site.register(Lesson)
 admin.site.register(Day)
+admin.site.register(Subject)
--- app/models.py
+++ app/models.py
@@ -19,19 +19,29 @@ class Week(models.Model):
         verbose_name_plural = 'Тиждні'
 
     name = models.CharField(max_length=100, default='week-name')
-    group = models.ForeignKey(Group, on_delete=models.CASCADE, null=True, related_name='weeks')
 
     def __str__(self):
-        return f'{self.name} - {self.group.name}'
+        return f'{self.name}'
 
 
 class Lesson(models.Model):
+    number_position = models.ForeignKey
+    subject = models.ForeignKey('Subject', on_delete=models.CASCADE, null=True)
+    tutor = models.ForeignKey(Tutor, on_delete=models.CASCADE, null=True)
+    day = models.ForeignKey('Day', on_delete=models.CASCADE, null=True)
+    week = models.ForeignKey(Week, on_delete=models.CASCADE, null=True)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f'{self.day}'
+
+
+class Subject(models.Model):
     class Meta:
         verbose_name = 'Предмет'
         verbose_name_plural = 'Предмети'
 
     name = models.CharField(max_length=200)
-    tutors = models.ManyToManyField(Tutor)
 
     def __str__(self):
         return self.name
@@ -41,33 +51,34 @@ class Day(models.Model):
     class Meta:
         verbose_name = 'День'
         verbose_name_plural = 'Дні'
+    name = models.CharField(max_length=100, default='')
 
-    TYPE_DAY_CHOICES = (
-        ('monday', 'понеділок'),
-        ('tuesday', 'вівторок'),
-        ('wednesday', 'середа'),
-        ('thursday', 'четверг'),
-        ('friday', 'пятниця')
-    )
-
-    day = models.CharField(max_length=20, choices=TYPE_DAY_CHOICES, default='')
-
-    week = models.ForeignKey(Week, on_delete=models.CASCADE, null=True, related_name='days')
-
-    lesson_1 = models.ForeignKey(Lesson, on_delete=models.CASCADE, null=True, blank=True, related_name='lesson1')
-    tutor_1 = models.ForeignKey(Tutor, on_delete=models.CASCADE, null=True, blank=True, related_name='tutor_1')
-
-    lesson_2 = models.ForeignKey(Lesson, on_delete=models.CASCADE, null=True, blank=True, related_name='lesson2')
-    tutor_2 = models.ForeignKey(Tutor, on_delete=models.CASCADE, null=True, blank=True, related_name='tutor_2')
-
-    lesson_3 = models.ForeignKey(Lesson, on_delete=models.CASCADE, null=True, blank=True, related_name='lesson3')
-    tutor_3 = models.ForeignKey(Tutor, on_delete=models.CASCADE, null=True, blank=True, related_name='tutor_3')
-
-    lesson_4 = models.ForeignKey(Lesson, on_delete=models.CASCADE, null=True, blank=True, related_name='lesson4')
-    tutor_4 = models.ForeignKey(Tutor, on_delete=models.CASCADE, null=True, blank=True, related_name='tutor_4')
-
-    lesson_5 = models.ForeignKey(Lesson, on_delete=models.CASCADE, null=True, blank=True, related_name='lesson5')
-    tutor_5 = models.ForeignKey(Tutor, on_delete=models.CASCADE, null=True, blank=True, related_name='tutor_5')
+    # TYPE_DAY_CHOICES =
+    #     ('monday', 'понеділок'),
+    #     ('tuesday', 'вівторок'),
+    #     ('wednesday', 'середа'),
+    #     ('thursday', 'четверг'),
+    #     ('friday', 'пятниця')
+    # )
+    #
+    # day = models.CharField(max_length=20, choices=TYPE_DAY_CHOICES, default='')
+    #
+    # week = models.ForeignKey(Week, on_delete=models.CASCADE, null=True, related_name='days')
+    #
+    # lesson_1 = models.ForeignKey(Lesson, on_delete=models.CASCADE, null=True, blank=True, related_name='lesson1')
+    # tutor_1 = models.ForeignKey(Tutor, on_delete=models.CASCADE, null=True, blank=True, related_name='tutor_1')
+    #
+    # lesson_2 = models.ForeignKey(Lesson, on_delete=models.CASCADE, null=True, blank=True, related_name='lesson2')
+    # tutor_2 = models.ForeignKey(Tutor, on_delete=models.CASCADE, null=True, blank=True, related_name='tutor_2')
+    #
+    # lesson_3 = models.ForeignKey(Lesson, on_delete=models.CASCADE, null=True, blank=True, related_name='lesson3')
+    # tutor_3 = models.ForeignKey(Tutor, on_delete=models.CASCADE, null=True, blank=True, related_name='tutor_3')
+    #
+    # lesson_4 = models.ForeignKey(Lesson, on_delete=models.CASCADE, null=True, blank=True, related_name='lesson4')
+    # tutor_4 = models.ForeignKey(Tutor, on_delete=models.CASCADE, null=True, blank=True, related_name='tutor_4')
+    #
+    # lesson_5 = models.ForeignKey(Lesson, on_delete=models.CASCADE, null=True, blank=True, related_name='lesson5')
+    # tutor_5 = models.ForeignKey(Tutor, on_delete=models.CASCADE, null=True, blank=True, related_name='tutor_5')
 
     def __str__(self):
-        return f'{self.day} - {self.week.group.name}'
+        return f'{self.name}'
