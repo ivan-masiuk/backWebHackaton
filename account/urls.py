cdd6f1f7037fd6677cc4a0de950f6a07122b7b2a
ivan-masiuk <vavolan@gmail.com>
2021-05-29 15:47:08 +0300

    add logout message

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
+from account.views import LogoutWithMessage
+
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
+    path('logout/', LogoutWithMessage.as_view(), name='logout'),
     path('password_change/',
          auth_views.PasswordChangeView.as_view(template_name='registration/password_change_form.html'),
          name='password_change'),
--- account/views.py
+++ account/views.py
@@ -1,1 +1,9 @@
-from django import apps
+from django.contrib import messages
+from django.contrib.auth import views as auth_views
+
+
+class LogoutWithMessage(auth_views.LogoutView):
+    def dispatch(self, request, *args, **kwargs):
+        response = super().dispatch(request, *args, **kwargs)
+        messages.add_message(request, messages.INFO, 'Ви успішно вийшли зі свого облікового запису!')
+        return response
