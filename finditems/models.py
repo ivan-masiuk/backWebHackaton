e27115c9060cdddeb5a9194c90fe5a155bacde80
Danil Vladimirov <danilvladimirovnkk@gmail.com>
2021-05-29 23:27:49 +0300

    created app finditems

--- finditems/models.py
+++ finditems/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Item(models.Model):
     title = models.CharField(max_length=200, default='')
     img = models.ImageField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.title
