480282c30971b837e36f409109f4bb7b155ca77e
Danil Vladimirov <danilvladimirovnkk@gmail.com>
2021-05-29 14:35:38 +0300

    group weeks

--- app/views.py
+++ app/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from app.models import Group
 
 
 def main_page(request):
     return render(request, 'base.html')
+
+
+def group_weeks(request, group_id):
+    context = {}
+    group = Group.objects.get(id=group_id)
+    weeks = group.weeks.all()
+    if weeks[0]:
+        week_1 = weeks[0]
+        context.update({'week_1': week_1})
+    if weeks[1]:
+        week_2 = weeks[1]
+        context.update({'week_2': week_2})
+    return render(request, 'weeks-page.html', context)
