68ad013ec711b7bac7d98777a0e87c8c10d1097f
ivan-masiuk <vavolan@gmail.com>
2021-05-29 15:03:36 +0300

    add email auth

--- account/models.py
+++ account/models.py
@@ -1,1 +1,29 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
+
+from phonenumber_field.modelfields import PhoneNumberField
+
+User._meta.get_field('email')._unique = True
+
+
+class AbstractUser(models.Model):
+    phone_number = PhoneNumberField(null=False, blank=False, unique=True)
+    TYPE_USER_CHOICES = (
+        ('tutor', 'Викладач'),
+        ('student', 'Студент'),
+    )
+    type_user = models.CharField(max_length=15, choices=TYPE_USER_CHOICES)
+
+    class Meta:
+        abstract = True
+
+
+class Tutor(AbstractUser):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE)
+
+
+class Student(AbstractUser):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE)
--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -140,3 +140,9 @@ LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+AUTHENTICATION_BACKENDS = [
+    'account.authentication.EmailAuthBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
