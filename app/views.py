b3c7d2d7880734e6ef16665fcedb4ff38f93f927
ivan-masiuk <vavolan@gmail.com>
2021-05-29 16:33:57 +0300

    communicate back and front in main_page

--- app/views.py
+++ app/views.py
@@ -2,8 +2,23 @@ from app.models import Group, Lesson
 from account.models import Tutor
 from django.shortcuts import render, redirect
 
+from django.contrib import messages
+
 
 def main_page(request):
+    action = request.POST.get('action')
+
+    if request.POST and action == 'Знайти':
+        name_group = request.POST['name_group']
+        groups = Group.objects.all()
+        for group in groups:
+            if group.name == name_group:
+                print(f"it works! we have group id={group.id}")
+                return redirect('group_weeks', group_id=group.id)
+        else:
+            messages.warning(request, "на жаль, такої групи не існує!")
+            return redirect('main_page')
+
     return render(request, 'start-page.html')
 
 
@@ -18,19 +33,6 @@ def group_weeks(request, group_id):
         week_2 = weeks[1]
         context.update({'week_2': week_2})
     return render(request, 'weeks-page.html', context)
-
-
-def test(request):
-    action = request.POST.get('action')
-
-    if request.POST and action == 'Знайти':
-        name_group = request.POST['name_group']
-        groups = Group.objects.all()
-        for group in groups:
-            if group.name == name_group:
-                return redirect('')
-        else:
-            return redirect('')
 
 
 def lesson_page(request, tutor_id, lesson_id):
